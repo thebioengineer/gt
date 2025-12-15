3850aa7c02b0318c9e0184dfdc35e62dcc7450ed
Romain Francois <romain@tada.science>
2025-12-15 09:50:12 +0100

    gt_to_word_contents() w/o officer

--- tests/testthat/helper-ooxml.R
+++ tests/testthat/helper-ooxml.R
@@ -1,3 +1,15 @@
+gt_to_word_contents <- function(gt, ...) {
+  temp_word_file <- withr::local_tempfile(fileext = ".docx")
+  gtsave(gt, temp_word_file, ..., as_word_func = as_word_ooxml)
+
+  temp_dir <- withr::local_tempfile()
+  unzip(temp_word_file, temp_dir)
+  doc <- xml2::read_xml(file.path(temp_dir, "word", "document.xml"))
+
+  xml_children(xml_children(doc))
+}
+
+
 read_xml_word_nodes <- function(x) {
   xml2::xml_children(suppressWarnings(xml2::read_xml(paste0(
     '<w:wrapper xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml">',
